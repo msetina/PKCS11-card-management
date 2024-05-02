3ee5fead9db2a95de0be9ce827509a2760c78e00
Miha Šetina <miha.setina@gmail.com>
2024-05-02 10:01:10 +0200

    simple test

---
+++ card_management_tests/test_first.py
@@ -0,0 +1,20 @@
+class TestFirst:
+
+    def test_create_keys_and_certs(self):
+        from pkcs11_card_management.smart_card import SmartCard
+
+        sc = SmartCard.from_profile("card_management_tests/softhsm_test.json")
+
+        personal_data = {
+            "country": "US",
+            "state": "United states",
+            "organizational unit": ["Secret unit"],
+            "organization": "The Firm",
+            "surname": "Secret",
+            "given name": "Joe",
+            "common name": "Joe Secret",
+            "email": "joe.secret@example.net",
+            "alt_names": ["joe.secret@example.net"],
+        }
+
+        sc.create_keys_and_certificates(personal_data, 30, None, False)
